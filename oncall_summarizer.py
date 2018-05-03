11b540d4f60a12bdb48b5062031b4ad70ef4b7e5
Yusheng Ding <davidthinkleding@Gmail.com>
2018-05-03 20:42:18 +0800

    center headers

--- oncall_summarizer.py
+++ oncall_summarizer.py
@@ -159,9 +159,9 @@ def getErrorReport(errorDict, errorSinceTime, currentTime):
 
 def getHtmlErrorReport(errorDict, errorSinceTime, currentTime):
     '''
-    Save error report into a specific dictionary HTML(libary input format)
+    Save error report into html format
     'Oncall Report: 2018-04-28 10:00 ~ 2018-04-29 10:00'
-    header = ["Error Type", "Occured Times", "Error Message Digest"]
+    header = ["Error Type", "Occured Times", "Occured Time", "Error Message Digest"]
     '''
     
     # for errorType, errorList in errorDict.items():
@@ -181,34 +181,43 @@ def getHtmlErrorReport(errorDict, errorSinceTime, currentTime):
     htmlReport = ''
     for errorType, errorList in errorDict.items():
         errFirstLine = True
-        for err in errorList:
-            if errFirstLine:
-                curline = '<tr>'
-                curline += '<td>' + errorType + '</td>'
-                curline += '<td  width="30"  align="center">' + str(len(errorDict[errorType])) + '</td>'
-                curline += '<td align="center">' + str(err.getErrorTime()) + '</td>'
-                curline += '<td>' + err.getDigestContent() + '</td>'
-                curline += '</tr>'
-                errFirstLine = False
-            else:
-                curline = '<tr>'
-                curline += '<td>' + ' ' + '</td>'
-                curline += '<td width="30"  align="center">' + ' ' + '</td>'
-                curline += '<td align="center">' + str(err.getErrorTime()) + '</td>'
-                curline += '<td>' + err.getDigestContent() + '</td>'
-                curline += '</tr>'
+        if not errorList:
+            curline = '<tr>'
+            curline += '<td>' + errorType + '</td>'
+            curline += '<td  width="30"  align="center">' + '0' + '</td>'
+            curline += '<td align="center">' + 'X' + '</td>'
+            curline += '<td align="center">' + '' + '</td>'
+            curline += '</tr>'
             htmlReport += curline
+        else:
+            for err in errorList:
+                if errFirstLine:
+                    curline = '<tr>'
+                    curline += '<td>' + errorType + '</td>'
+                    curline += '<td  width="30"  align="center">' + str(len(errorDict[errorType])) + '</td>'
+                    curline += '<td align="center">' + str(err.getErrorTime()) + '</td>'
+                    curline += '<td>' + err.getDigestContent() + '</td>'
+                    curline += '</tr>'
+                    errFirstLine = False
+                else:
+                    curline = '<tr>'
+                    curline += '<td>' + ' ' + '</td>'
+                    curline += '<td width="30"  align="center">' + ' ' + '</td>'
+                    curline += '<td align="center">' + str(err.getErrorTime()) + '</td>'
+                    curline += '<td>' + err.getDigestContent() + '</td>'
+                    curline += '</tr>'
+                htmlReport += curline
     html = '''
     <html>
         <head></head>
         <body>
-            <h1>Oncall Report: ''' + 'Oncall Summery From {0} To {1}'.format(errorSinceTime.strftime ("%Y-%m-%d %H:%M"),currentTime.strftime ("%Y-%m-%d %H:%M"))+ '''</h1>
+            <h1 align="center">Oncall Report: ''' + 'Oncall Summery From {0} To {1}'.format(errorSinceTime.strftime("%Y-%m-%d %H:%M"),currentTime.strftime("%Y-%m-%d %H:%M"))+ '''</h1>
             <table border="1">
                 <tr>
-                    <td>Error Type</td>
-                    <td width="30"  align="center">Occured Times</td>
-                    <td align="center">Occured Time</td>
-                    <td>Error Message Digest</td>
+                    <td align="center"><strong>Error Type</strong></td>
+                    <td width="30"  align="center"><strong>Occured Times</strong></td>
+                    <td align="center"><strong>Occured Time</strong></td>
+                    <td align="center"><strong>Error Message Digest</strong></td>
                 </tr>
                 '''+htmlReport+'''
             </table>
@@ -251,7 +260,7 @@ def main():
     #     errorSinceTime = currentTime -  datetime.timedelta(days = last_N_days)
     # else:
     #     errorSinceTime = currentTime -  datetime.timedelta(days = last_N_days) + datetime.timedelta(hours = 10)
-    errorSinceTime = currentTime -  datetime.timedelta(days = last_N_days) - datetime.timedelta(days = 7)
+    errorSinceTime = currentTime -  datetime.timedelta(days = last_N_days) - datetime.timedelta(days = 5)
     errorSource = ['root@yay161.bjs.p1staff.com', 'data-ops@tantan.com','prometheus@p1.com']
     errorDict = getErrorDict(config,errorSinceTime,errorSource,currentTime)
     #summaryReport = getErrorReport(errorDict, errorSinceTime, currentTime)
